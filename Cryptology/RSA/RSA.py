804c31801c8ea34378e03293909419ddc312a0e1
Joe Shaw <63563424+Joseph112358@users.noreply.github.com>
2022-07-23 01:18:24 +0100

    Fixed crashing.
    Fixed crashing, but encryption/decryption stops working from letter O onwards. I think I need to generate a better key.

--- Cryptology/RSA/RSA.py
+++ Cryptology/RSA/RSA.py
@@ -1,31 +1,31 @@
 # Alphabet and associated code.
 letters ={
-  'A' : 2,
-  'B' : 3,
-  'C' : 4,
-  'D' : 5,
-  'E' : 6,
-  'F' : 7,
-  'G' : 8,
-  'H' : 9,
-  'I' : 10,
-  'J' : 11,
-  'K' : 12,
-  'L' : 13,
-  'M' : 14,
-  'N' : 15,
-  'O' : 16,
-  'P' : 17,
-  'Q' : 18,
-  'R' : 19,
-  'S' : 20,
-  'T' : 21,
-  'U' : 22,
-  'V' : 23,
-  'W' : 24,
-  'X' : 25,
-  'Y' : 26,
-  'Z' : 27
+  'A' : 0,
+  'B' : 1,
+  'C' : 2,
+  'D' : 3,
+  'E' : 4,
+  'F' : 5,
+  'G' : 6,
+  'H' : 7,
+  'I' : 8,
+  'J' : 9,
+  'K' : 10,
+  'L' : 11,
+  'M' : 12,
+  'N' : 13,
+  'O' : 14,
+  'P' : 15,
+  'Q' : 16,
+  'R' : 17,
+  'S' : 18,
+  'T' : 19,
+  'U' : 20,
+  'V' : 21,
+  'W' : 22,
+  'X' : 23,
+  'Y' : 24,
+  'Z' : 25
 }
 
 # Method for converting number to letter.
@@ -48,13 +48,14 @@ def rsa_algorithm_string(key, string):
       return_string += cipher_char
     return(return_string)
 
+
+
 encryption_key = (5,14)
 decryption_key = (11,14)
 
 # Letters with wrong encryption: O,P,Q,R,S,T,U,V,W,X,Y,Z
-# Letters causing crash: M,N
 # Write algorithm to generate keys
-encryption_result = rsa_algorithm_string(encryption_key,"Z")
+encryption_result = rsa_algorithm_string(encryption_key,"ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 print(encryption_result)
 decryption_result = rsa_algorithm_string(decryption_key, encryption_result)
 print(decryption_result)
